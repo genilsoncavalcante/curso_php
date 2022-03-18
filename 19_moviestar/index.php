defdd72a207975e2045a542665c929f7f6bda67d
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-03-18 12:03:36 -0300

    estilização de login e registro

--- 19_moviestar/index.php
+++ 19_moviestar/index.php
@@ -1,83 +1,54 @@
 <?php
-require_once("globals.php");
-require_once("db.php");
+require_once("templates/header.php");
 ?>
-
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>MovieStar</title>
-   <link rel="short icon" href="<?php echo $BASE_URL ?>/img/moviestar.ico">
-   <!-- CSS -->
-   <link rel="stylesheet" href="<?php echo $BASE_URL ?>/css/styles.css">
-   <!-- BOOTSTRAP -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-   <!-- FONT AWESOME -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-   <header>
-      <nav id="main-navbar" class="navbar navbar-expand-lg">
-         <a href="<?php echo $BASE_URL ?>" class="navbar-brand">
-            <img src="<?php echo $BASE_URL ?>/img/logo.svg" alt="MovieStar" id="logo">
-            <span id="moviestar-title">MovieStar</span>
-         </a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars"></i>
-         </button>
-         <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-            <button type="submit" class="btn my-2 my-sm-0">
-               <i class="fas fa-search"></i>
-            </button>
-         </form>
-         <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav">
-               <li class="nav-item">
-                  <a href="<?php echo $BASE_URL ?>/auth.php" class="nav-link">Entrar / Cadastrar</a>
-               </li>
-            </ul>
+<div id="main-container" class="container-fluid">
+   <div class="col-md-12">
+      <div class="row" id="auth-row">
+         <div class="col-md-4" id="login-container">
+            <h2>Entrar</h2>
+            <form action="" method="POST">
+               <input type="hidden" name="type" value="login">
+               <div class="form-group">
+                  <label for="email">E-mail:</label>
+                  <input type="email" name="email" id="email" class="form-control" placeholder="Digite se e-mail">
+               </div>
+               <div class="form-group">
+                  <label for="password">Senha:</label>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="Digite sua senha">
+               </div>
+               <input type="submit" value="Entrar" class="btn card-btn">
+            </form>
          </div>
-      </nav>
-   </header>
-
-   <div id="main-container" class="container-fluid">
-      <h1>Corpo do Site</h1>
+         <div class="col-md-4" id="login-container">
+            <h2>Criar Conta</h2>
+            <form action="" method="POST">
+               <input type="hidden" name="type" value="register">
+               <div class="form-group">
+                  <label for="email">E-mail:</label>
+                  <input type="email" name="email" id="email" class="form-control" placeholder="Digite se e-mail">
+               </div>
+               <div class="form-group">
+                  <label for="name">Nome:</label>
+                  <input type="text" name="name" id="name" class="form-control" placeholder="Digite o seu nome">
+               </div>
+               <div class="form-group">
+                  <label for="lastname">Sobrenome:</label>
+                  <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Digite o seu sobrenome">
+               </div>
+               <div class="form-group">
+                  <label for="password">Senha:</label>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="Digite sua senha">
+               </div>
+               <div class="form-group">
+                  <label for="confirmpassword">Confirmação de senha:</label>
+                  <input type="password" name="confirmpassword" id="confirmpassword" class="form-control" placeholder="Confirme sua senha">
+               </div>
+               <input type="submit" value="Registrar" class="btn card-btn">
+            </form>
+         </div>
+      </div>
    </div>
-
-   <footer id="footer">
-      <div class="social-container">
-         <ul>
-            <li>
-               <a href="#"> <i class="fab fa-facebook-square"></i> </a>
-            </li>
-            <li>
-               <a href="#"> <i class="fab fa-instagram"></i> </a>
-            </li>
-            <li>
-               <a href="#"> <i class="fab fa-youtube"></i> </a>
-            </li>
-         </ul>
-      </div>
-      <div id="footer-links-container">
-         <ul>
-            <li><a href="#">Adicionar filme</a></li>
-            <li><a href="#">Adicionar crítica</a></li>
-            <li><a href="#">Entrar / Registrar</a></li>
-         </ul>
-      </div>
-      <p>&copy; 2022 Genilson Cavalcante</p>
-   </footer>
-
-   <!-- BOOTSTRAP JS -->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-
-</html>
+</div>
+<?php
+require_once("templates/footer.php");
+?>
