5ee79a94a36f253b529cd967767a853fb2232dd5
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-03-28 17:42:42 -0300

    review em anamento

--- 19_moviestar/index.php
+++ 19_moviestar/index.php
@@ -10,10 +10,17 @@ $latestMovies = $movieDao->getLatestMovies();
 
 $actionMovies = $movieDao->getMoviesByCategory("Ação");
 
+$dramaMovies = $movieDao->getMoviesByCategory("Drama");
+
 $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+
+$fantasyMovies = $movieDao->getMoviesByCategory("Fantasia / Ficção");
+
+$romanceMovies = $movieDao->getMoviesByCategory("Romance");
 
 ?>
 <div id="main-container" class="container-fluid">
+
    <h2 class="section-title">Filmes novos</h2>
    <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
    <div class="movies-container">
@@ -24,6 +31,7 @@ $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
          <p class="empty-list">Ainda não há filmes cadastrados!</p>
       <?php endif; ?>
    </div>
+
    <h2 class="section-title">Ação</h2>
    <p class="section-description">Veja os melhores filmes de ação</p>
    <div class="movies-container">
@@ -34,6 +42,18 @@ $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
          <p class="empty-list">Ainda não há filmes de ação cadastrados!</p>
       <?php endif; ?>
    </div>
+
+   <h2 class="section-title">Drama</h2>
+   <p class="section-description">Veja os melhores filmes de Drama</p>
+   <div class="movies-container">
+      <?php foreach ($dramaMovies as $movie) : ?>
+         <?php require("templates/movie_card.php"); ?>
+      <?php endforeach; ?>
+      <?php if (count($dramaMovies) === 0) : ?>
+         <p class="empty-list">Ainda não há filmes de drama cadastrados!</p>
+      <?php endif; ?>
+   </div>
+
    <h2 class="section-title">Comédia</h2>
    <p class="section-description">Veja os melhores filmes de comédia</p>
    <div class="movies-container">
@@ -44,6 +64,29 @@ $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
          <p class="empty-list">Ainda não há filmes de comédia cadastrados!</p>
       <?php endif; ?>
    </div>
+
+   <h2 class="section-title">Fantasia / Ficção</h2>
+   <p class="section-description">Veja os melhores filmes de Fantasia / Ficção</p>
+   <div class="movies-container">
+      <?php foreach ($fantasyMovies as $movie) : ?>
+         <?php require("templates/movie_card.php"); ?>
+      <?php endforeach; ?>
+      <?php if (count($fantasyMovies) === 0) : ?>
+         <p class="empty-list">Ainda não há filmes de Fantasia / Ficção cadastrados!</p>
+      <?php endif; ?>
+   </div>
+
+   <h2 class="section-title">Romance</h2>
+   <p class="section-description">Veja os melhores filmes de Romance</p>
+   <div class="movies-container">
+      <?php foreach ($romanceMovies as $movie) : ?>
+         <?php require("templates/movie_card.php"); ?>
+      <?php endforeach; ?>
+      <?php if (count($romanceMovies) === 0) : ?>
+         <p class="empty-list">Ainda não há filmes de Romance cadastrados!</p>
+      <?php endif; ?>
+   </div>
+
 </div>
 <?php
 require_once("templates/footer.php");
